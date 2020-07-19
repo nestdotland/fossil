fc2ad14ac6767ae7c0adfc75d679f67ef2b72149
tate <tateflyer1@gmail.com>
2020-07-18 21:47:48 -0400

    feat: Added footer and updated favicon

--- src/archive/src/App.vue
+++ src/archive/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="nest" id="app">
     <Gallery />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
 
 export default {
   name: "App",
   components: {
     Gallery,
+    Footer,
   },
 };
 </script>
